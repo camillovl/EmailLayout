9a970eb88ddccfc0f86471e9d74afb1e6e3257c6
camillovl <camillovl10@gmail.com>
2022-09-26 01:17:19 -0300

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style = {styles.header}>
+      <View style = {styles.conjunto}>
+          <Image style={styles.userimg} source = {require('C:/Users/camil/Documents/Dispositivos moveis/Unidade1/EmailLayout/IntegrantesUra/Camillo.jpg')}></Image>
+          <View style={styles.textoseicone}>
+          <Text> Nome </Text>
+          <Text> Titulo </Text>
+          <Text> Assunto </Text>
+          <View style = {styles.icones}>
+          <FontAwesome5 name = "star" size = {14} color = "gray" />
+          </View>
+          </View>        
+      </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: 'red',
   },
+  header:{
+    height: 80,
+    backgroundColor: 'pink'
+  },
+  conjunto:{
+    height:90,
+    padding:10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'green'
+  },
+  userimg: {
+    height: 60,
+    width: 60,
+    borderRadius: 25,
+  },
+  textoseicone: {
+    marginLeft: 5,
+    justifyContent: 'space-between',
+  },
+
+  
 });
